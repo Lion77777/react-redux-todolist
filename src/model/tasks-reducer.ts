07c2aef530de772dab14dd4420e4fa2c0fc81b7e
Lion77777 <leon.avetisyan.1993@mail.ru>
2025-11-19 01:29:05 +0400

    Overwrite tasks' action creators by RTK's createAction function

--- src/model/tasks-reducer.ts
+++ src/model/tasks-reducer.ts
@@ -1,6 +1,7 @@
-import {v1} from 'uuid'
-import type {Task, TasksState} from '../App'
-import type {CreateTodolistAction, DeleteTodolistAction} from './todolists-reducer'
+import { v1 } from 'uuid'
+import type { Task, TasksState } from '../App'
+import { type CreateTodolistAction, type DeleteTodolistAction } from './todolists-reducer'
+import { createAction } from '@reduxjs/toolkit'
 
 const initialState: TasksState = {}
 
@@ -13,26 +14,26 @@ export const tasksReducer = (state: TasksState = initialState, action: Actions):
       }
     }
     case 'create_task': {
-      const newTask: Task = {title: action.payload.title, isDone: false, id: v1()}
-      return {...state, [action.payload.todolistId]: [newTask, ...state[action.payload.todolistId]]}
+      const newTask: Task = { title: action.payload.title, isDone: false, id: v1() }
+      return { ...state, [action.payload.todolistId]: [newTask, ...state[action.payload.todolistId]] }
     }
     case "change_task_status": {
       return {
         ...state,
-        [action.payload.todolistId]: state[action.payload.todolistId].map(task => task.id === action.payload.taskId ? {...task, isDone: action.payload.isDone} : task)
+        [action.payload.todolistId]: state[action.payload.todolistId].map(task => task.id === action.payload.taskId ? { ...task, isDone: action.payload.isDone } : task)
       }
     }
     case "change_task_title": {
       return {
         ...state,
-        [action.payload.todolistId]: state[action.payload.todolistId].map(task => task.id === action.payload.taskId ? {...task, title: action.payload.title} : task)
+        [action.payload.todolistId]: state[action.payload.todolistId].map(task => task.id === action.payload.taskId ? { ...task, title: action.payload.title } : task)
       }
     }
     case "create_todolist": {
-      return {...state, [action.payload.id]: []}
+      return { ...state, [action.payload.id]: [] }
     }
     case "delete_todolist": {
-      const newState = {...state}
+      const newState = { ...state }
       delete newState[action.payload.id]
       return newState
     }
@@ -41,21 +42,10 @@ export const tasksReducer = (state: TasksState = initialState, action: Actions):
   }
 }
 
-export const deleteTaskAC = (payload: { todolistId: string, taskId: string }) => {
-  return {type: 'delete_task', payload} as const
-}
-
-export const createTaskAC = (payload: { todolistId: string, title: string }) => {
-  return {type: 'create_task', payload} as const
-}
-
-export const changeTaskStatusAC = (payload: { todolistId: string, taskId: string, isDone: boolean }) => {
-  return {type: 'change_task_status', payload} as const
-}
-
-export const changeTaskTitleAC = (payload: { todolistId: string, taskId: string, title: string }) => {
-  return {type: 'change_task_title', payload} as const
-}
+export const createTaskAC = createAction<{ todolistId: string, title: string }>('tasks/createTask')
+export const deleteTaskAC = createAction<{ todolistId: string, taskId: string }>('tasks/deleteTask')
+export const changeTaskStatusAC = createAction<{ todolistId: string, taskId: string, isDone: boolean }>('tasks/changetTaskStatus')
+export const changeTaskTitleAC = createAction<{ todolistId: string, taskId: string, title: string }>('tasks/changetTaskTitle')
 
 export type DeleteTaskAction = ReturnType<typeof deleteTaskAC>
 export type CreateTaskAction = ReturnType<typeof createTaskAC>
@@ -63,9 +53,9 @@ export type ChangeTaskStatusAction = ReturnType<typeof changeTaskStatusAC>
 export type ChangeTaskTitleAction = ReturnType<typeof changeTaskTitleAC>
 
 type Actions =
-    | DeleteTaskAction
-    | CreateTaskAction
-    | ChangeTaskStatusAction
-    | ChangeTaskTitleAction
-    | CreateTodolistAction
-    | DeleteTodolistAction
+  | DeleteTaskAction
+  | CreateTaskAction
+  | ChangeTaskStatusAction
+  | ChangeTaskTitleAction
+  | CreateTodolistAction
+  | DeleteTodolistAction
