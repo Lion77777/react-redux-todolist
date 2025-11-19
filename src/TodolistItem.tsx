d2b496234e70d6c3d25589e5e884d9c4d75bd0e9
Lion77777 <leon.avetisyan.1993@mail.ru>
2025-11-20 02:51:25 +0400

    Create FilterButtons Component and move todolists' filtering layout and logic into FilterButtons

--- src/TodolistItem.tsx
+++ src/TodolistItem.tsx
@@ -1,10 +1,6 @@
-import Box from '@mui/material/Box'
-import Button from '@mui/material/Button'
-import { containerSx } from './TodolistItem.styles'
-import { useAppDispatch } from './common/hooks/useAppDispatch'
-import { changeTodolistFilterAC } from './model/todolists-reducer'
 import { TodolistTitle } from './TodolistTitle'
 import { Tasks } from './Tasks'
+import { FilterButtons } from './FilterButtons'
 
 type Props = {
   todolist: Todolist
@@ -26,38 +22,13 @@ export type FilterValues = 'all' | 'active' | 'completed'
 
 export type TasksState = Record<string, Task[]>
 
-export const TodolistItem = (props: Props) => {
-  const {
-    todolist: { id, filter },
-  } = props
-
-  const dispatch = useAppDispatch()
-
-  const changeFilter = (filter: FilterValues) => {
-    dispatch(changeTodolistFilterAC({ id, filter }))
-  }
+export const TodolistItem = ({todolist}: Props) => {
 
   return (
     <div>
-      <TodolistTitle todolist={props.todolist} />
-      <Tasks todolist={props.todolist} />
-      <Box sx={containerSx}>
-        <Button variant={filter === 'all' ? 'outlined' : 'text'}
-          color={'inherit'}
-          onClick={() => changeFilter('all')}>
-          All
-        </Button>
-        <Button variant={filter === 'active' ? 'outlined' : 'text'}
-          color={'primary'}
-          onClick={() => changeFilter('active')}>
-          Active
-        </Button>
-        <Button variant={filter === 'completed' ? 'outlined' : 'text'}
-          color={'secondary'}
-          onClick={() => changeFilter('completed')}>
-          Completed
-        </Button>
-      </Box>
+      <TodolistTitle todolist={todolist} />
+      <Tasks todolist={todolist} />
+      <FilterButtons todolist={todolist}/>
     </div>
   )
 }
