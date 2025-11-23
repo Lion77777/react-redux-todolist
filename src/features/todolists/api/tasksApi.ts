2e8b4755712d0cb0d89cabf9db5f3a225b09340a
Lion77777 <leon.avetisyan.1993@mail.ru>
2025-11-23 12:39:04 +0400

    Specify requested tasks' type

--- src/features/todolists/api/tasksApi.ts
+++ src/features/todolists/api/tasksApi.ts
@@ -1,7 +1,8 @@
 import { instance } from "@/common/instance/instance"
+import { GetTasksResponse } from "./tasksApi.types"
 
 export const tasksApi = {
     getTasks(todolistId: string) {
-        return instance.get(`/todo-lists/${todolistId}/tasks`)
+        return instance.get<GetTasksResponse>(`/todo-lists/${todolistId}/tasks`)
     }
 }
